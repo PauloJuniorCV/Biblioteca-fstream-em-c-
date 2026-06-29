302421d946dd11698e9e7e140968ab97f7652022
Paulo <paulojunior_pp@hotmail.com>
2026-06-29 11:38:28 -0300

    atualização

--- Sistema_de_Supermercado.cpp
+++ Sistema_de_Supermercado.cpp
@@ -98,20 +98,20 @@ void pesquisar(){
 
         arquivo.close();
         
-        if(!achou){//caso nao encontre o produto
+        if(!achou){//caso não encontre o produto
             cout<<"Produto nao encontrado\n";
             return;
         }
     }
 
-    else if(opcaoDePesquisa==2){//buscar pelo produto pelo codigo
+    else if(opcaoDePesquisa==2){//buscar pelo produto pelo código
         
         arquivo.open("produtos.txt", fstream::in);
         cout<<"Digite o codigo do produto\n";
         cin.ignore();
         getline(cin,codigoBuscar);
 
-        bool achou=false;//para se o produto nao for achado conseguir avidar
+        bool achou=false;//para se o produto não for achado conseguir avisar
 
         while(getline(arquivo, linha)){ 
             if(linha.empty()) continue; //impede que o produto receba o valor do enter (vazio)
@@ -129,7 +129,7 @@ void pesquisar(){
         }
 
         arquivo.close();
-        if(!achou){//caso nao encontre o produto
+        if(!achou){//caso não encontre o produto
             cout<<"Produto nao encontrado\n";
             return;
         }
@@ -137,7 +137,7 @@ void pesquisar(){
     int opcao;
     do{ 
         cout<<endl<<"Deseja editar editar, excluir o produto ou sair?\n" 
-        <<"[1]=Sair, [2]=Editar, [3]=Excluir\n"; //saber se precisa ser modificado algo ou nao
+        <<"[1]=Sair, [2]=Editar, [3]=Excluir\n"; //saber se precisa ser modificado algo ou não
         cin>>opcao;
 
     }while(opcao != 1 && opcao != 2 && opcao != 3);   
@@ -150,7 +150,7 @@ void pesquisar(){
         
         char resposta;
         
-        do{//caso o usuario digite outra eltra
+        do{//caso o usuário digite outra letra
             cout<<"O que deseja editar?\n"
             <<"[N]=Nome [C]=Codigo [V]=Validade [Q]=Quantidade \n";//saber qual dado deseja editar
             cin>>resposta;
@@ -161,13 +161,13 @@ void pesquisar(){
         
         fstream copia;
         copia.open("novo.txt",fstream::out);
-        /*criei um novo documento txt. Como nao é possivel apagar dados, iremos criar uma copia para armazenar as antigas
-        informações, so nao iremos copiar a linha que foi editada, em vez disso, escreveremos as novas informaçoes na linha*/
+        /*cria um novo documento txt. Como não é possivel apagar dados, iremos criar uma copia para armazenar as antigas
+        informações, só não iremos copiar a linha que foi editada, em vez disso, escreveremos as novas informações na linha*/
 
-        bool apagado=false; //quando for falsa copia as informaçoes no novo documento, quando for verdadeira para de copiar
+        bool apagado=false; //quando for falsa copia as informações no novo documento, quando for verdadeira para de copiar
         
-        int contador=0; /*contador para colocar os numeros de linhas que nao precisa ser copiado para o novo documento
-        (essas sao as linhas que escreveremos manualmente porque o usuario deseja editar)*/
+        int contador=0; /*contador para colocar os números de linhas que não precisa ser copiado para o novo documento
+        (essas são as linhas que escreveremos manualmente porque o usuario deseja editar)*/
 
         switch(resposta){
             case 'n':
@@ -180,7 +180,7 @@ void pesquisar(){
                 while(getline(arquivo,linha)){ 
                     
                     if(linha==produtoV){
-                        apagado=true; //quando apagado vira true nao é copiado a linha
+                        apagado=true; //quando "apagado" vira true não é copiado a linha
                     }
 
                     if(apagado==false){
@@ -188,7 +188,7 @@ void pesquisar(){
                     }
 
                     else if(apagado==true){
-                        copia<<"Produto: "<<novoNome<<endl; // no lugar que a linha nao copiou é colocado a mudança
+                        copia<<"Produto: "<<novoNome<<endl; // no lugar que a linha não copiou é colocado a mudança
                     }
                     apagado=false; //depois é liberado para continuar copiando as linhas e colando normalmente
                 }
@@ -197,7 +197,7 @@ void pesquisar(){
             
             case 'c':
             case 'C':{
-                cout<<"Qual o novo codigo?\n";//editar o codigo do produto 
+                cout<<"Qual o novo codigo?\n";//editar o código do produto 
                 string novoCodigo;
                 cin.ignore();
                 getline(cin, novoCodigo);
@@ -231,13 +231,13 @@ void pesquisar(){
                     
                     if(linha==codigo){ 
                         apagado=true;
-                    }/*porque para de copiar e colar na linha do codigo e nao no da validade? 
-                    porque se colocar na validade ele pode mudar a validade de outros produtos que vencem no mesmo dia 
-                    do produto escolhido, pois as linha serao iguais (o mesmo serve para quando for editar o volume). 
-                    Colocando o codigo, ou o nome, não tem risco disso acontecer, ja que os codigos e os nomes 
-                    de todos os produtos sao diferentes*/ 
+                    }/*por que parar de copiar e colar na linha do código e não no da validade? 
+                    porque se colocar na validade, ele pode mudar a validade de outros produtos que vencem no mesmo dia 
+                    do produto escolhido, pois as linha serão iguais nesse caso (o mesmo serve para quando for editar o volume). 
+                    Colocando o código, ou o nome, não tem risco disso acontecer, ja que os códigos e os nomes 
+                    de todos os produtos são diferentes*/ 
 
-                    if(contador==2){ //contador que faz o codigo parar de copiar por 2 linhas (linha do codigo e da validade)
+                    if(contador==2){ //contador que faz o código parar de copiar por 2 linhas (linha do codigo e da validade)
                         apagado=false;
                     }
 
@@ -246,8 +246,8 @@ void pesquisar(){
                     }
 
                     if(apagado==true && contador==0){
-                        copia<<codigo<<endl; //coloca novamente o codigo ja que ele parou de copiar nessa linha
-                        copia<<"Validade: "<<data<<endl; //coloca a informaçao nova que foi editada
+                        copia<<codigo<<endl; //coloca novamente o código ja que ele parou de copiar nessa linha
+                        copia<<"Validade: "<<data<<endl; //coloca a informação nova que foi editada
 
                     }
                     if(apagado==true){
@@ -271,7 +271,7 @@ void pesquisar(){
 
                     if(contador==3){ 
                         apagado=false;
-                    } /* 3 linhas nao seram copiadas, a do codigo, da validade(que esta entre a linha do codigo e 
+                    } /* 3 linhas não seram copiadas, a do código, da validade(que esta entre a linha do código e 
                     entre a que queremos editar) e a linha da quantidade(a que queremos editar)*/
 
                     if(apagado==false){
@@ -296,7 +296,7 @@ void pesquisar(){
         //os dois arquivos foram fechados
 
         remove("produtos.txt"); //o antigo que estava sendo copiado foi apagado
-        rename("novo.txt", "produtos.txt"); /*o novo que os dados foram passados e editados nas linhas pedidas 
+        rename("novo.txt", "produtos.txt"); /*o "novo" que os dados foram passados e editados nas linhas pedidas 
         foi renomeado com o mesmo nome do artigo que foi apagado*/
 
         cout<<"Produto editado com sucesso!\n";
@@ -322,26 +322,26 @@ void pesquisar(){
             bool apagado=false; //selecionar as linha ques seram reescritas no novo documento
             while(getline(arquivo, linha)){
                 if(linha==produtoV){
-                    apagado=true; //selecionar as linha ques nao seram reescritas no novo documento
+                    apagado=true; //selecionar as linha ques não seram reescritas no novo documento
                 }
                 
                 if(apagado==false){
-                    copia<<linha<<endl; //copia todas as informaçoes novamente menos a que usuario quer excluir
+                    copia<<linha<<endl; //copia todas as informações novamente, menos a que o usuário quer excluir
                 }
 
-                if(linha.empty()){ //selecionando linha em branco
+                if(linha.empty()){ //selecionando linha em branco (as que não tem nada)
                     apagado=false; //selecionar as linha para voltarem a serem copiadas para no novo documento
                 }
             }
             arquivo.close();
             copia.close();
 
-            remove("produtos.txt"); //deleta o antigo documento que a informaçao que queria excluir existe
+            remove("produtos.txt"); //deleta o antigo documento que a informação que queria excluir existe
 
             rename("novo.txt", "produtos.txt"); 
-            //renomeou o documento, que nao foi copiado o produto que queria excluir, para o mesmo nome do que foi excluido 
+            //renomeou o documento, que não copiou o produto que queria excluir, para o mesmo nome do que foi excluido 
             
-            cout<<"Produto excluido com sucesso!\n";//avisa ao usuario que o conteudo foi "apagado"/excluido
+            cout<<"Produto excluido com sucesso!\n";//avisa ao usuário que o conteudo foi "apagado"/excluido
         }   
         else{
             cout<<"Resposta invalida\n";
@@ -355,13 +355,13 @@ void alerta(){
     arquivo.open("produtos.txt", fstream::in);
     
     string linha;
-    int contador=1; //para saber em qual linha está e poder salvar a linha correta com as suas respectivas informaçoes
+    int contador=1; //para saber em qual linha o loop está e poder salvar a linha correta com as suas respectivas informações
     string produto, lotes;
-    bool executado=false;//saber se nao tem produtos com o estoque baixo
+    bool executado=false;//saber caso não tenha produtos com o estoque baixo
     
     while(getline(arquivo,linha)){
                 
-        if(contador==1){ //quando o contador for 1 ele sempre vai estar na linha do nome do produto, pois é a primeira informaçao
+        if(contador==1){ //quando o contador for 1 ele sempre vai estar na linha do nome do produto, pois é a primeira informação
             produto=linha;
         }
 
@@ -378,14 +378,14 @@ void alerta(){
                     
         if(linha==""){ 
             contador=0;
-        }/*quando encontra linha vazia (que tem antes de cada produto exeto o primeiro, por isso o contador começa em 1) 
-        o contador reinicia para fazer a analise do proximo produto*/
+        }/*quando encontra linha vazia (que tem antes de cada produto exceto o primeiro, por isso o contador começa em 1) 
+        o contador reinicia para fazer a analise do próximo produto*/
 
         contador++;
 
     }
             
-    if(!executado){
+    if(!executado){ //se o executado for falso significa que não encontrou nenhum produto com estoque baixo
         cout<<"Nao ha produtos com estoque baixo\n";
     }
 
